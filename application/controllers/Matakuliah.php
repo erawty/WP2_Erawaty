953edae71ea0f10bc094ba746b068b4c760f2451
erawty <eraw38750@gmail.com>
2023-10-08 00:40:12 +0700

    TUGAS PERTEMUAN 3

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -5,7 +5,7 @@ class Matakuliah extends CI_Controller
     public function index()
     { 
         
-        $this->load->view('view-form-matakuliah'); 
+        $this->load->helper('view-form-matakuliah'); 
     } 
     public function cetak() 
     { 
